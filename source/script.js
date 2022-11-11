f2ea0b2fb90191ec7657e8be5d3fc595525267e5
Blair <blairmunroakusa@wp.computer>
2022-11-10 15:00:00 -0900

    improve dashed visual indicator

--- source/script.js
+++ source/script.js
@@ -14,23 +14,49 @@
  * @param {} response - the response from the API, an object with an attribute of `malicious`
  */
 function handleAPIResponse(response) {
+	
+	// prepare visual indicator canvas object
+	//
+	// NOTE: The ideal way to accomplish this is to inject CSS and script via
+	//		 chrome.scripting.executeScript and chrome.scripting.insertCSS.
+	//		 I cannot however, figure out how to get chrome.scripting to work
+	//		 outside the background env. This is ugly and should be refactored.
+	//
+	var box = document.createElement("canvas");
+	var boxWrapper = document.createElement("div");
+	box.style.bottom = "auto";
+	box.style.position = "fixed";
+	box.style.width = "100%";
+	box.style.height = "100%";
+	box.style.zIndex = "40000";
+	box.style.pointerEvents = "none";
+	
+	
     if (response == null)
     {
         console.log("API Unresponsive. Cannot verify safety of: " +
                     window.location.href +
                     ".");
-        document.body.style.border = "4px dashed yellow";
+		// finish up wrapped html and inject into body
+		box.style.border = "6px dashed yellow";
+		boxWrapper.appendChild(box);
+		document.body.insertBefore(boxWrapper, document.body.childNodes[0]);
+
     } else if (response.malicious == false)
     {
         console.log("URL " +
                     window.location.href +
                     " not classified as malicious.");
+
     } else if (response.malicious == true)
     {
         alert("The URL you are visiting: " + 
               window.location.href + 
               " is potentially malicious! Proceed at your own risk.");
-        document.body.style.border = "4px dashed red";
+		// finish up wrapped html and inject into body
+		box.style.border = "6px dashed red";
+		boxWrapper.appendChild(box);
+		document.body.insertBefore(boxWrapper, document.body.childNodes[0]);
     }
 }
 
