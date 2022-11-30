f463ccb9d4bfab8309ee346acebd6cbbfb528369
Dan TS <dan@interlock.network>
2022-11-30 12:34:46 -0800

    updates styles in index.html and styles.css * also updates badge.js to modern vernacular JS

--- source/static/badge.js
+++ source/static/badge.js
@@ -1,24 +1,30 @@
 window.addEventListener("load", function() {
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
-  function myTabClicks(tabClickEvent) {
-	console.log('click');
-		for (var i = 0; i < myTabs.length; i++) {
+	const myTabs = document.querySelectorAll("ul.nav-tabs > li");
+
+	function myTabClicks(tabClickEvent) {
+		for (let i = 0; i < myTabs.length; i++) {
 			myTabs[i].classList.remove("active");
 		}
-		var clickedTab = tabClickEvent.currentTarget;
+
+		let clickedTab = tabClickEvent.currentTarget;
+
 		clickedTab.classList.add("active");
 		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
+
+		const myContentPanes = document.querySelectorAll(".tab-pane");
+
+		for (let i = 0; i < myContentPanes.length; i++) {
 			myContentPanes[i].classList.remove("active");
 		}
-		var anchorReference = tabClickEvent.target;
-		var activePaneId = anchorReference.getAttribute("href");
-		var activePane = document.querySelector(activePaneId);
+
+		const anchorReference = tabClickEvent.target;
+		const activePaneId = anchorReference.getAttribute("href");
+		const activePane = document.querySelector(activePaneId);
+
 		activePane.classList.add("active");
 	}
-	for (i = 0; i < myTabs.length; i++) {
+
+	for (let i = 0; i < myTabs.length; i++) {
 		myTabs[i].addEventListener("click", myTabClicks)
 	}
 });
