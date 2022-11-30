ac7faf8f98c0e7724c23ce96f18708d78f974393
Dan TS <dan@interlock.network>
2022-11-30 15:26:46 -0800

    added toggle switch for data sharing * new styles in style.css * JS for toggle in badge.js * HTML in index.html

--- source/static/badge.js
+++ source/static/badge.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function() {
 	const myTabs = document.querySelectorAll("ul.nav-tabs > li");
+	const toggle = this.document.getElementById("switch");
+	const notYet = this.document.getElementById("not-yet");
 
 	function myTabClicks(tabClickEvent) {
 		for (let i = 0; i < myTabs.length; i++) {
@@ -27,4 +29,16 @@ window.addEventListener("load", function() {
 	for (let i = 0; i < myTabs.length; i++) {
 		myTabs[i].addEventListener("click", myTabClicks)
 	}
+
+	function toggleReset() {
+		if (toggle.checked) {
+			setTimeout(() => {
+				toggle.checked = false;
+				notYet.style.display = 'inherit';
+			}, 250);
+
+		}
+	}
+
+	toggle.addEventListener("click", toggleReset)
 });
