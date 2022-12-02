f8ed766e907499392d8ea02099a4587d04567e65
Dan TS <dan@interlock.network>
2022-12-01 16:56:56 -0800

    adds labels, tightes up look n feel in CSS

--- source/script.js
+++ source/script.js
@@ -1,31 +1,26 @@
 /**
- * This script is the main content script used by Threatslayer. It is
+ * This script is the main content script used by ThreatSlayer. It is
  * executed every time the user visits a page.
  */
 
 /**
  * This function handles the response from the API. If the response
- * indicates that a URL is malicious, the user will be prompted, and
- * the page will be bordered with red. If the URL is *not* malicious,
+ * indicates that a URL is malicious, the user will be prompted 
+ * in a new window. If the URL is *not* malicious,
  * a console message will be logged. If the API is unresponsive, or
- * fails to provide a response for some other reason, the page is
- * bordered in yellow to inform the user to proceed with caution.
+ * fails to provide a response for some other reason, a console 
+ * message will be logged. 
  *
  * @param {} response - the response from the API, an object with an attribute of `malicious`
  */
 function handleAPIResponse(response) {
-    if (response == null)
-    {
-        console.log("API Unresponsive. Cannot verify safety of: " +
-                    window.location.href +
-                    ".");
-    } else if (response.malicious == false)
-    {
-        console.log("URL " +
-                    window.location.href +
-                    " not classified as malicious.");
-    } else if (response.malicious == true)
-    {
+    let { href } = window.location;
+
+    if (response === null) {
+        console.log(`API Unresponsive. Cannot verify safety of URL ${href} .`);
+    } else if (response.malicious === false) {
+        console.log(`URL ${href} not classified as malicious.`);
+    } else if (response.malicious === true) {
         chrome.runtime.sendMessage("displayWarningBanner");
     }
 }
@@ -39,10 +34,9 @@ function handleAPIResponse(response) {
 chrome.runtime.sendMessage(
     {contentScriptQuery: "queryURL", url: window.location.href},
     function (response) {
-        if (response != undefined && response != "") {
+        if (response !== undefined && response !== "") {
             handleAPIResponse(response);
-        }
-        else {
+        } else {
             handleAPIResponse(null);
         }
     });
