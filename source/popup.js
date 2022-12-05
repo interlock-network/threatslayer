ebfe3698430a002069f4fa569f7403e65c28d5ad
John Mercouris <john@mercouris.email>
2022-12-05 12:14:02 -0600

    popup.js: delete uneeded file

--- source/popup.js
+++
@@ -1,7 +0,0 @@
-const closeButton = document.getElementById('threatslayer-close');
-
-closeButton.addEventListener("click", () => {
-    const popupContainer = document.getElementById('threatslayer-container');
-    window.postMessage('test')
-    console.log('after test')
-})
