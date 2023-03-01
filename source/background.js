490b45aa547630baac6495cf0c7a4fdd30a22240
Dan TS <dan@interlock.network>
2023-03-01 12:15:45 -0700

    background.js: checks for beta AI flag before querying pipeline

--- source/background.js
+++ source/background.js
@@ -2,11 +2,12 @@
  * This is the main background script for Threatslayer.
  */
 const APIKey = "threatslayer-api-key";
-const baseAPIUrl = `http://octahedron.interlock.network`;
+const baseAPIUrl = `https://octahedron.interlock.network`;
+const betaBaseAPIUrl = `https://beta.octahedron.interlock.network`;
 const defaultConfig = {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({key: APIKey})
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ key: APIKey }),
 };
 
 /**
@@ -14,120 +15,83 @@ const defaultConfig = {
  * scripts. It is invoked by script.js using
  * `chrome.runtime.sendMessage`.
  */
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.contentScriptQuery === "queryURL") {
-            chrome.storage.local.get(["totalURLsVisited"])
-                .then((result) => {
-                    let totalURLsVisited = result.totalURLsVisited || 0;
-                    totalURLsVisited++;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    let selectedBaseAPIUrl = baseAPIUrl;
 
-                    chrome.storage.local.set({ "totalURLsVisited": totalURLsVisited })
-                        .then(() => {
-                            console.log(`Total URLs set to: ${totalURLsVisited}`);
-                        });
+    if (request.contentScriptQuery === "queryURL") {
+        chrome.storage.local.get(["totalURLsVisited"]).then((result) => {
+            let totalURLsVisited = result.totalURLsVisited || 0;
+            totalURLsVisited++;
+
+            chrome.storage.local
+                .set({ totalURLsVisited: totalURLsVisited })
+                .then(() => {
+                    console.log(`Total URLs set to: ${totalURLsVisited}`);
                 });
+        });
 
-            fetch("https://octahedron.interlock.network/malicious_p",
-                {
-                    ...defaultConfig,
-                    body: JSON.stringify({key: APIKey,
-                        url: request.url})
-                }
-            )
-                .then(response => response.json())
-                .then(response => sendResponse(response))
-                .catch(error => console.log(`Error getting malicious URL: ${error}`));
+        chrome.storage.sync.get("betaAISelected", function (data) {
+            if (data.betaAISelected && data.betaAISelected === true) {
+                console.log(
+                    "Querying beta AI Threat Detection at",
+                    betaBaseAPIUrl
+                );
+                selectedBaseAPIUrl = betaBaseAPIUrl;
+            }
 
-            return true;
-        } else if (request === "displayWarningBanner") {
-            chrome.storage.local.get(["totalMaliciousURLsVisited"])
-                .then((result) => {
-                    let totalMaliciousURLsVisited = result.totalMaliciousURLsVisited || 0;
-                    totalMaliciousURLsVisited++;
+            fetch(`${selectedBaseAPIUrl}/malicious_p`, {
+                ...defaultConfig,
+                body: JSON.stringify({ key: APIKey, url: request.url }),
+            })
+                .then((response) => response.json())
+                .then((response) => sendResponse(response))
+                .catch((error) =>
+                    console.log(`Error getting malicious URL: ${error}`)
+                );
+        });
 
-                    chrome.storage.local.set({"totalMaliciousURLsVisited": totalMaliciousURLsVisited })
-                        .then(() => {
-                            console.log("Total malicious URLs set to:" + totalMaliciousURLsVisited);
-                        });
-                });
+        return true;
+    } else if (request === "displayWarningBanner") {
+        chrome.storage.local
+            .get(["totalMaliciousURLsVisited"])
+            .then((result) => {
+                let totalMaliciousURLsVisited =
+                    result.totalMaliciousURLsVisited || 0;
+                totalMaliciousURLsVisited++;
 
-            // inject styling
-            chrome.scripting.insertCSS(
-                {
-                    target: {tabId: sender.tab.id},
-                    files: ["banner.css"]
-                });
-            // execute script
-            chrome.scripting.executeScript(
-                {
-                    target: {tabId: sender.tab.id},
-                    files: ["banner.js"]
-                })
-                .then(response => sendResponse(response))
-                .catch(error => console.log(error));
+                chrome.storage.local
+                    .set({
+                        totalMaliciousURLsVisited: totalMaliciousURLsVisited,
+                    })
+                    .then(() => {
+                        console.log(
+                            "Total malicious URLs set to:" +
+                                totalMaliciousURLsVisited
+                        );
+                    });
+            });
 
-            return true;
-        }
-    });
+        // inject styling
+        chrome.scripting.insertCSS({
+            target: { tabId: sender.tab.id },
+            files: ["banner.css"],
+        });
+        // execute script
+        chrome.scripting
+            .executeScript({
+                target: { tabId: sender.tab.id },
+                files: ["banner.js"],
+            })
+            .then((response) => sendResponse(response))
+            .catch((error) => console.log(error));
 
-/**
- * This listener is responsible for getting KPIs from the pipeline.
- */
-chrome.runtime.onMessage.addListener(
-    function(request, _sender, sendResponse) {
-        if (request === "malicious_urls_scanned_count") {
-            fetch(`https://octahedron.interlock.network/malicious_urls_scanned_count`, defaultConfig)
-                .then(async response => {
-                    const total = await response.text();
-
-                    return total;
-                })
-                .then(response => sendResponse(response))
-                .catch(error => {
-                    console.log(`Error getting malicious URL count: ${error}`);
-                    
-                    return 'Error';
-                });
-
-            return true;
-        } else if (request === "urls_scanned_count") {
-            fetch(`https://octahedron.interlock.network/urls_scanned_count`, defaultConfig)
-                .then(async response => {
-                    const total = await response.text();
-
-                    return total;
-                })
-                .then(response => sendResponse(response))
-                .catch(error => {
-                    console.log(`Error getting URL scanned count: ${error}`);
-                    
-                    return 'Error';
-                });
-
-            return true;
-        } else if (request === "user_count") {
-            fetch(`https://octahedron.interlock.network/user_count`, defaultConfig)
-                .then(async response => {
-                    const total = await response.text();
-
-                    return total;
-                })
-                .then(response => sendResponse(response))
-                .catch(error => {
-                    console.log(`Error getting user count: ${error}`);
-                    
-                    return 'Error';
-                });
-
-            return true;
-        }
-    });
-
+        return true;
+    }
+});
 
 /**
  * This listener opens our survey in a new tab when users uninstall
  */
 chrome.runtime.setUninstallURL(
-    'https://docs.google.com/forms/d/e/1FAIpQLSeo1gW6Sg_ITlAXxbTXliQdab2qt1cLBzu45mXpz-XJ8O1KPg/viewform',
+    "https://docs.google.com/forms/d/e/1FAIpQLSeo1gW6Sg_ITlAXxbTXliQdab2qt1cLBzu45mXpz-XJ8O1KPg/viewform"
 );
