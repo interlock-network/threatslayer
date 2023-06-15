602f4ab4d880097091f47ae9d4228b3a1ce379e3
Dan TS <dan@interlock.network>
2023-06-15 14:12:08 -0700

    LineOfText.vue: formatting changes

--- src/pages/components/subcomponents/LineOfText.vue
+++ src/pages/components/subcomponents/LineOfText.vue
@@ -1,9 +1,7 @@
 <template>
-    <div>
-        <span class="line-of-text" :style="style">{{ msg }}
-            <slot />
-        </span>
-    </div>
+    <span class="line-of-text" :style="style">{{ msg }}
+        <slot />
+    </span>
 </template>
 
 <script>
@@ -33,21 +31,22 @@ export default {
             return this.error ?
                 { color: "red" }
                 : this.footer
-                    ? { "font-size": "0.8rem", "color": "gray" }
+                    ? {
+                        "color": "gray",
+                        "display": "inline",
+                        "font-size": "0.8rem"
+                    }
                     : this.error ?
                         { color: "red" }
                         : this.seed ?
                             {
                                 "align-items": "center",
-                                "background-color": "#D0D4D9",
                                 color: "black",
-                                display: "flex",
                                 "font-family": "monospace",
-                                height: "5rem",
-                                "padding-left": "1rem",
-                                width: "100%",
+                                "padding-top": "1rem",
+                                width: "100%"
                             }
-                            : {}
+                            : { width: "100%" }
         },
     },
 };
@@ -59,6 +58,5 @@ export default {
     display: inline-flex;
     opacity: 85%;
     margin-bottom: 0.5rem;
-    width: 100%;
 }
 </style>
