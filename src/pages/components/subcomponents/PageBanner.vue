a593806437c2314aec1f84dd6673d99732bd1e4a
Dan TS <dan@interlock.network>
2023-06-16 14:03:13 -0700

    PageBanner.vue: adds new component for page headline

---
+++ src/pages/components/subcomponents/PageBanner.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="page-banner">{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    name: "PageBanner",
+    props: {
+        msg: String,
+    },
+};
+</script>
+
+<style>
+.page-banner {
+    color: #E7E9EA;
+    display: block;
+    font-size: 2rem;
+    width: 100%;
+}
+</style>
