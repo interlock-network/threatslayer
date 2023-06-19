a93d8e0a526e72c6458dbd9c8042c256a4d71fe3
Dan TS <dan@interlock.network>
2023-06-19 16:19:06 -0700

    ConnectWalletView.vue CreateAccountView.vue: changed filename

---
+++ src/pages/components/CreateAccountView.vue
@@ -0,0 +1,18 @@
+<template>
+    <StepView header="Connect Your Wallet" text="To receive $ILOCK token, connect your Aleph Zero-compatible wallet.">
+        <slot />
+    </StepView>
+</template>
+
+<script>
+import StepView from "./subcomponents/StepView.vue";
+
+export default {
+    name: "CreateAccountView",
+    components: {
+        StepView,
+    }
+};
+</script>
+
+<style></style>
