6543c1d5b99b5b60ea1f9e0a09485cab7df877bd
Dan TS <dan@interlock.network>
2023-06-19 10:32:17 -0700

    CreateInstructions.vue: update to reflect simplified flow

--- src/pages/components/CreateInstructions.vue
+++ src/pages/components/CreateInstructions.vue
@@ -1,15 +1,5 @@
 <template>
-    <table>
-        <tr style="width: 100%">
-            <td style="width: 66%">
-                <LineOfText msg="We recommend SubWallet if you don't already have an Aleph Zero-compatible wallet."
-                    instruction />
-            </td>
-            <td style="width: 33%">
-                <slot />
-            </td>
-        </tr>
-    </table>
+    <LineOfText msg="After you create a wallet, click 'Connect Wallet' and enter your wallet address." instruction />
 </template>
 
 <script>
@@ -19,9 +9,6 @@ export default {
     name: "CreateInstructions",
     components: {
         LineOfText,
-    },
-    props: {
-        active: Boolean
     },
 };
 </script>
