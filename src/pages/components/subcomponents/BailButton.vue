7aa8e77833e07259ea3f91446da9787c19b3a92a
Dan TS <dan@interlock.network>
2023-06-20 09:27:24 -0700

    BailButton.vue: adds Bail button to go next to Submit button

---
+++ src/pages/components/subcomponents/BailButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="bail-button" @click="bailOut()">
+        Maybe later
+    </button>
+</template>
+
+<script>
+export default {
+    name: "BailButton",
+    props: {
+        active: Boolean,
+    },
+    methods: {
+        bailOut() {
+            window.open('https://interlock.network');
+        }
+    }
+};
+</script>
+
+<style>
+.bail-button {
+    border: none;
+    background: #060708;
+    color: gray;
+    pointer-events: initial;
+    /* font-size: 1.25rem; */
+    padding-top: 1rem;
+    width: 50%;
+}
+
+.bail-button:focus {
+    border: 3px solid #d0d4d9;
+}
+</style>
