9f1f0426712053bfb22306c882c0e58c829772d7
Dan TS <dan@interlock.network>
2023-06-21 16:55:31 -0700

    App.vue: fixes (?) getting registered and logged info from chrome storage state * Also includes two buttons for testing purposes * One toggles registered state, the other logged in state

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,22 @@
   <div id="app-container">
     <div id="sidebar-nav">
       <h3 id="sidebar-banner"><span class="name-start">Threat</span><span class="name-end">Slayer</span></h3>
-      <div id="sidebar-earn" v-if="!registered && !loggedIn" class="sidebar-item selected-sidebar-item"
+      <div id="sidebar-earn" v-if="showRegisterPage" class="sidebar-item selected-sidebar-item"
         @click="selectPage('earn', $event)">Start Earning</div>
-      <div id="sidebar-login" class="sidebar-item" @click="selectPage('login', $event)">Login</div>
+      <div id="sidebar-login" v-if="showLoginPage" class="sidebar-item" @click="selectPage('login', $event)">Login</div>
       <div id="sidebar-collect" class="sidebar-item" @click="selectPage('collect', $event)">Collect</div>
       <div id="sidebar-slayCount" class="sidebar-item" @click="selectPage('slayCount', $event)">Slay Count</div>
       <div id="sidebar-about" class="sidebar-item" @click="selectPage('about', $event)">About Us</div>
-      <div id="sidebar-privacy" class="sidebar-item" @click="selectPage('privacy', $event)">Privacy & Security</div>
+      <div id="sidebar-privacy" class="sidebar-item" @click="selectPage('privacy', $event)">Privacy <img
+          class="link-button-icon" src="/src/assets/images/external_link.png"></div>
       <div id="sidebar-options" class="sidebar-item" @click="selectPage('options', $event)">Options</div>
+      <!-- TODO delete these two buttons -->
+      <br />
+      <br />
+      <br />
+      <button @click="_toggleRegistered" style="pointer-events: initial;">Toggle Register</button>
+      <br />
+      <button @click="_toggleLogin" style="pointer-events: initial;">Toggle Login</button>
     </div>
     <div id="page-container">
       <EarnPage v-if="currentPage === 'earn'" />
@@ -49,54 +57,87 @@ export default {
     };
   },
   mounted() {
-    this.isRegistered();
     this.isLoggedIn();
+    this.isRegistered();
   },
   computed: {
+    showRegisterPage() {
+      return !this.registered;
+    },
+    showLoginPage() {
+      return !this.loggedIn;
+    }
   },
   methods: {
-    isLoggedIn() {
-      // if unregistered, switch to earn page
-      try {
-        chrome.storage.local.get('loggedIn', response => {
-          const { loggedIn = false } = response;
+    // TODO delete this
+    _toggleLogin() {
+      chrome.storage.local.set({ 'loggedIn': !this.loggedIn });
+      this.isLoggedIn();
+    },
+    // TODO delete this
+    _toggleRegistered() {
+      chrome.storage.local.set({ 'registered': !this.registered });
+      this.isRegistered();
+    },
+    async isLoggedIn() {
+      // if logged in, hide register and login pages
+      // then navigate to the profile page
 
-          if (loggedIn) {
-            this.loggedIn = loggedIn;
-          }
-        });
+      try {
+        const storageLoggedIn = await chrome.storage.local.get('loggedIn')
+          .then(response => {
+            const { loggedIn } = response;
+            console.log('loggedIn', loggedIn);
+            return loggedIn;
+          });
+
+        this.loggedIn = storageLoggedIn;
+        // TODO uncomment
+        // if (storageLoggedIn) {
+        //   this.currentPage = 'profile';
+        // }
       } catch (err) {
         console.log("isLoggedIn err", err);
       }
     },
-    isRegistered() {
-      // if registered, show slaycount
+    async isRegistered() {
       try {
-        chrome.storage.local.get('registered', response => {
-          const { registered = false } = response;
+        const storageRegistered = await chrome.storage.local.get('registered')
+          .then(response => {
+            const { registered } = response;
 
-          if (registered) {
-            this.registered = registered;
-            this.currentPage = slayCount;
-          }
-        });
+            return registered;
+          });
+
+        this.registered = storageRegistered;
+
+        // if registered (but not logged in), switch to login page
+        // otherwise user stays on the "earn" page via the mounted() lifecycle call
+        if (storageRegistered) {
+          this.currentPage = 'login';
+        }
       } catch (err) {
         console.log("isRegistered err", err);
       }
     },
     selectPage(page, event) {
-      // change the main page content
-      this.currentPage = page;
+      if (page === 'privacy') {
+        // TODO update to real privacy page URL
+        window.open('https://interlock.network');
+      } else {
+        // change the main page content
+        this.currentPage = page;
 
-      // highlight the selected sidebar item
-      const sidebarItems = document.getElementsByClassName('sidebar-item');
+        // highlight the selected sidebar item
+        const sidebarItems = document.getElementsByClassName('sidebar-item');
 
-      for (let i = 0; i < sidebarItems.length; i++) {
-        const item = sidebarItems[i];
-        item.classList.remove('selected-sidebar-item');
+        for (let i = 0; i < sidebarItems.length; i++) {
+          const item = sidebarItems[i];
+          item.classList.remove('selected-sidebar-item');
+        }
+
+        event.target.classList.add('selected-sidebar-item');
       }
-
-      event.target.classList.add('selected-sidebar-item');
     }
   }
 }
@@ -160,6 +201,11 @@ body {
   width: 100%;
 }
 
+.link-button-icon {
+  height: 1rem;
+  /* padding-left: 0.25rem; */
+}
+
 .name-start {
   color: #3b8de8;
 }
@@ -172,6 +218,10 @@ body {
   line-height: 2rem;
   opacity: 0.9;
   pointer-events: initial;
+}
+
+.hidden-item {
+  display: none;
 }
 
 .selected-sidebar-item {
