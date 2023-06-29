8a97caa5804ec99765254dc9bffa804bb6ee0d86
Dan TS <dan@interlock.network>
2023-06-29 13:10:06 -0700

    OptionsPage.vue: updated styling for consistency

--- src/pages/OptionsPage.vue
+++ src/pages/OptionsPage.vue
@@ -1,22 +1,21 @@
 <template>
     <PageBanner msg="Extension Options" />
-    <OptionsView>
-        <div class="options-container">
-            <input type="checkbox" id="beta-ai-checkbox" @click="betaClassifier($event)" tabindex="1" />
-            <label for="beta-ai-checkboxbox">
-                Enable beta AI Threat Detection (developers only)
-            </label>
-        </div>
-    </OptionsView>
+    <LineOfText msg="Select from the following:" instruction />
+    <div class="options-container">
+        <input type="checkbox" id="beta-ai-checkbox" @click="betaClassifier($event)" tabindex="1" />
+        <label for="beta-ai-checkboxbox">
+            Enable beta AI Threat Detection (developers only)
+        </label>
+    </div>
 </template>
 <script>
-import OptionsView from "./components/OptionsView.vue";
+import LineOfText from "./components/subcomponents/LineOfText.vue";
 import PageBanner from "./components/subcomponents/PageBanner.vue";
 
 export default {
     name: 'OptionsPage',
     components: {
-        OptionsView,
+        LineOfText,
         PageBanner
     },
     data() {
