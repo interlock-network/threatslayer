34c187f9b2a9182de5cd07ddd27e116cea269d49
Dan TS <dan@interlock.network>
2023-06-29 16:34:06 -0700

    LoginPage.vue: removed cruft * code around toggling login vs forgot pw flow * disused classes and other styling

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -15,7 +15,7 @@
     <LineOfText msg="Password" bold />
     <input id="login-password" class="input-field-text password-input" :type="passwordInputType" v-model.trim="password"
         placeholder="Password" tabindex="4" :style="passwordInputStyle" />
-    <button @click="togglePasswordInputType" class="small-button" id="show-toggle-button" style="right: 0.5rem;"
+    <button @click="togglePasswordInputType" class="small-button" id="show-toggle-button" style="right: 3.25rem;"
         tabindex="5">
         {{ passwordInputType === 'password' ? 'Show' : 'Hide' }}
     </button>
@@ -36,10 +36,8 @@
 import { debounce } from 'debounce';
 import { findEmailError, findNonAlphanumericChars } from "../utilities";
 
-import ForgotLoginButton from "./components/subcomponents/ForgotLoginButton.vue";
 import ForgotPasswordButton from "./components/subcomponents/ForgotPasswordButton.vue";
 import LineOfText from "./components/subcomponents/LineOfText.vue";
-import GetLoginInfoButton from "./components/subcomponents/GetLoginInfoButton.vue";
 import LoginButton from "./components/subcomponents/LoginButton.vue";
 import PageBanner from "./components/subcomponents/PageBanner.vue";
 
@@ -49,16 +47,13 @@ const errorStyle = {
 };
 const maxPasswordLength = 16; // number of characters
 
-
 export default {
     name: 'EarnPage',
     props: {
         changePage: Function,
     },
     components: {
-        ForgotLoginButton,
         ForgotPasswordButton,
-        GetLoginInfoButton,
         LineOfText,
         LoginButton,
         PageBanner,
@@ -90,25 +85,6 @@ export default {
     methods: {
         clearUsernameErrors() {
             this.usernameErrorMessage = '';
-        },
-        toggleForgot() {
-            this.forgotUsernamePassword = !this.forgotUsernamePassword;
-
-
-            if (this.forgotUsernamePassword) {
-                setTimeout(() => {
-                    const emailInput = document.getElementById('forgot-info-email');
-
-                    emailInput.focus();
-                }, 100);
-            } else {
-                setTimeout(() => {
-                    const usernameInput = document.getElementById('login-username-or-email');
-
-                    usernameInput.focus();
-                }, 100);
-
-            }
         },
         togglePasswordInputType() {
             this.passwordInputType = this.passwordInputType === 'password' ? 'text' : 'password';
@@ -161,26 +137,10 @@ input:focus {
     float: left;
 }
 
-/* #show-toggle-button {
-    border: none;
-    color: #963cf5;
-    margin-top: 0.35rem;
-} */
-
 .login-page-submit-button-container {
     color: #d0d4d9;
     width: 400px;
 }
-
-/* .login-page-submit-button {
-    background: #060708;
-    cursor: pointer;
-    float: left;
-    font-size: 1.5rem;
-    pointer-events: initial;
-    width: 50%;
-    padding: 0.5rem 0.75rem;
-} */
 
 .disabled {
     border: 1px solid #d0d4d9;
@@ -198,13 +158,6 @@ input:focus {
 
 .submit-button-error {
     color: red;
-}
-
-.small-button {
-    background-color: #060708;
-    border: 1px solid #d0d4d9;
-    color: #d0d4d9;
-    pointer-events: initial;
 }
 </style>
   
