d3216da7a52a2d1519bd239de941f4e7e8222ed8
Dan TS <dan@interlock.network>
2023-07-06 15:26:14 -0700

    PageFooter.vue: footer now full width, more padding

--- src/pages/components/PageFooter.vue
+++ src/pages/components/PageFooter.vue
@@ -1,26 +1,27 @@
 <template>
-    <div id="page-footer">
-        <table style="width: 100%;">
-            <tr>
-                <td style="min-width: 10%;"></td>
-                <td style="width: 40%;">
-                    <img id="logo-white" src="/src/assets/images/logo_white.svg"><br />
-                    <div id="left-hand-block">
-                        <LineOfText msg="Interlock © 2023 All Rights Reserved" footer />
-                    </div>
-                </td>
-                <td style="width: 40%; line-height: 1.25rem;">
-                    <LineOfText msg="Company" footer bold />
-                    <FooterLink msg="Privacy Policy" /><br />
-                    <FooterLink msg="Do Not Sell My Personal Information." /><br />
-                    <FooterLink msg="Terms of Service" />
-                </td>
-                <td>
-                    <div style="width: 10%;"></div>
-                </td>
-            </tr>
-        </table>
-
+    <div id="footer-container">
+        <div id="page-footer">
+            <table style="width: 100%;">
+                <tr>
+                    <td style="width: 32%;"></td>
+                    <td style="width: 18%;">
+                        <img id="logo-white" src="/src/assets/images/logo_white.svg"><br />
+                        <div id="left-hand-block">
+                            <LineOfText msg="Interlock © 2023 All Rights Reserved" footer />
+                        </div>
+                    </td>
+                    <td style="width: 18%; line-height: 1.25rem;">
+                        <LineOfText msg="Company" footer bold />
+                        <FooterLink msg="Privacy Policy" /><br />
+                        <FooterLink msg="Do Not Sell My Personal Information." /><br />
+                        <FooterLink msg="Terms of Service" />
+                    </td>
+                    <td>
+                        <div style="width: 32%;"></div>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -38,6 +39,16 @@ export default {
 </script>
 
 <style>
+#footer-container {
+    background-color: #0F0818;
+    bottom: 0;
+    box-shadow: 0px 6px 46px 0px rgba(187, 0, 253, 0.30);
+    /* left: 2rem; */
+    height: 17%;
+    position: absolute;
+    width: 100%;
+}
+
 #left-hand-block {
     transform: translate(0, -14px);
 }
@@ -50,9 +61,10 @@ export default {
 
 #page-footer {
     color: gray;
-    display: flex;
+    /* display: flex; */
     font-size: 0.75rem;
-    padding-left: 2rem;
+    /* padding-left: 2rem; */
+    padding-top: 0.6rem;
     position: absolute;
     top: 1rem;
     width: 100%;
