7bc61be6ac60c65bd272c6ab7750acea3762bf69
Dan TS <dan@interlock.network>
2023-07-12 11:53:23 -0700

    WalletPage.vue: adds new "Create Wallet" page

---
+++ src/pages/WalletPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <PageBanner msg="Create a Wallet">
+        <img class="banner-icon" src="/src/assets/images/wallet.png">
+    </PageBanner>
+    <LineOfText msg="In order to use ThreatSlayer, you will need to create an Aleph Zero compatible wallet." />
+    <LineOfText msg="Please click the link to be redirected to a Chrome Web Page extension to create a SubWallet." />
+    <br />
+    <br />
+    <WalletButton tabindex="2" />
+</template>
+<script>
+import LineOfText from "./components/subcomponents/LineOfText.vue";
+import PageBanner from "./components/subcomponents/PageBanner.vue";
+import WalletButton from "./components/subcomponents/WalletButton.vue";
+
+export default {
+    name: 'AboutPage',
+    components: {
+        LineOfText,
+        PageBanner,
+        WalletButton
+    },
+    data() {
+        return {};
+    },
+    methods: {}
+}
+</script>
+  
+<style></style>
+  
