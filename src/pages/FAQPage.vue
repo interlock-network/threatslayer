a09d1b68eb7f461476ae1c9f6f4b9ecd7154f5fc
Dan TS <dan@interlock.network>
2023-07-19 15:16:34 -0700

    FAQPage.vue: removes unneeded social media icons at botton

--- src/pages/FAQPage.vue
+++ src/pages/FAQPage.vue
@@ -23,17 +23,6 @@
                 target="_blank">source
                 code on GitbHub</a> and see exactly what it does.</span>
     </LineOfText>
-    <div style="pointer-events: initial;">
-        <a href="https://bit.ly/ilocktwitter" target="_blank" class="socials-icon" style="padding-left: -1rem;">
-            <img class="social-icon" src="/src/assets/images/social_twitter.png">
-        </a>
-        <a href="https://bit.ly/intldiscord" target="_blank" class="socials-icon">
-            <img class="social-icon" src="/src/assets/images/social_discord.png">
-        </a>
-        <a href="https://bit.ly/intlchat" target="_blank" class="socials-icon">
-            <img class="social-icon" src="/src/assets/images/social_telegram.png">
-        </a>
-    </div>
 </template>
 <script>
 import LineOfText from "./components/subcomponents/LineOfText.vue";
