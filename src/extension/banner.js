2bc0522e4e1209fb8704f80fd674832f7e8ad13a
Dan TS <dan@interlock.network>
2023-07-21 09:39:25 -0700

    banner.js: refactors to new block screen

--- src/extension/banner.js
+++ src/extension/banner.js
@@ -1,44 +1,63 @@
 /**
  * This script is injected into website if url is found to be malicious.
  */
-(function() {
+(function () {
     // create elements
-    var body = document.body;
-    var image = document.createElement('img');
-    var bannerWrapper = document.createElement('div');
-    var box = document.createElement('canvas');
-    var boxWrapper = document.createElement('div');
-    var banner = document.createElement('div');
-    var bannerTitle = document.createElement('p');
-    var bannerText = document.createElement('p');
-    var closeButton = document.createElement('a');
+    const body = document.body;
+    const bannerWrapper = document.createElement('div');
+    const box = document.createElement('canvas');
+    const boxWrapper = document.createElement('div');
+    const banner = document.createElement('div');
+    const textBox = document.createElement('div');
+    const image = document.createElement('img');
+    const bannerTitle = document.createElement('p');
+    const bannerText = document.createElement('p');
+    const warning = document.createElement('p');
+    const button = document.createElement('button');
+
     // link elements to stylesheet
     bannerWrapper.id = 'warningbanner-wrapper';
     box.id = 'box';
     banner.id = 'warningbanner';
-    bannerTitle.id = 'warningbanner-text';
-    bannerText.id = 'warningbanner-text';
-    closeButton.id = "warningbanner-close";
-    image.id = 'hand48';
+    textBox.id = 'text-box';
+    image.id = 'threatslayer_logo';
+    warning.id = 'warning';
+    bannerTitle.class = 'warningbanner-text';
+    bannerText.class = 'warningbanner-text';
+    button.id = 'dismiss-button';
+
+    // add background image
+    backgroundImageURL = chrome.runtime.getURL('grid_background.png');
+    banner.style.background = `url(${backgroundImageURL}) repeat`;
+
     // generate logo URL
-    imageURL = chrome.runtime.getURL('hand48.png');
+    imageURL = chrome.runtime.getURL('threatslayer_logo.png');
     image.setAttribute('src', imageURL);
-    // warning banner title
+
+    // "Warning!" / "This website may be malicious." / "Close the tab unless you know this site is safe!"
+    warning.appendChild(document.createTextNode(chrome.i18n.getMessage("warning")));
     bannerTitle.appendChild(document.createTextNode(chrome.i18n.getMessage("warning_header")));
-    // warning banner text
     bannerText.appendChild(document.createTextNode(chrome.i18n.getMessage("warning_text")));
+
+    // "Trust This Site" button
+    button.innerHTML = '<span id="dismiss-button-text">Trust This Site</span>';
+
     // build banner and underlying box
     banner.appendChild(image);
-    banner.appendChild(bannerTitle);
-    banner.appendChild(bannerText);
-    banner.appendChild(closeButton);
+    banner.append(textBox);
+    textBox.appendChild(warning);
+    textBox.appendChild(bannerTitle);
+    textBox.appendChild(bannerText);
+    textBox.appendChild(button);
     bannerWrapper.appendChild(banner);
     boxWrapper.appendChild(box);
+
     // insert box and banner into page
     body.insertBefore(bannerWrapper, body.childNodes[0]);
     body.insertBefore(boxWrapper, body.childNodes[0]);
+
     // on click to close banner, remove banner and box
-    closeButton.onclick = function() {
+    button.onclick = function () {
         body.removeChild(bannerWrapper);
         body.removeChild(boxWrapper);
     };
