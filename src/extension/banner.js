4f86308620af3818e921e781202a6fef28bcd64e
Dan TS <dan@interlock.network>
2023-07-21 16:30:59 -0700

    banner.js: checks allowlist before showing block screen

--- src/extension/banner.js
+++ src/extension/banner.js
@@ -2,63 +2,85 @@
  * This script is injected into website if url is found to be malicious.
  */
 (function () {
-    // create elements
-    const body = document.body;
-    const bannerWrapper = document.createElement('div');
-    const box = document.createElement('canvas');
-    const boxWrapper = document.createElement('div');
-    const banner = document.createElement('div');
-    const textBox = document.createElement('div');
-    const image = document.createElement('img');
-    const bannerTitle = document.createElement('p');
-    const bannerText = document.createElement('p');
-    const warning = document.createElement('p');
-    const button = document.createElement('button');
+    chrome.storage.local.get(["allowlist"]).then((result) => {
+        const allowlist = result.allowlist || [];
+        const currentURL = document.URL;
 
-    // link elements to stylesheet
-    bannerWrapper.id = 'warningbanner-wrapper';
-    box.id = 'box';
-    banner.id = 'warningbanner';
-    textBox.id = 'text-box';
-    image.id = 'threatslayer_logo';
-    warning.id = 'warning';
-    bannerTitle.class = 'warningbanner-text';
-    bannerText.class = 'warningbanner-text';
-    button.id = 'dismiss-button';
+        if (!allowlist.includes(currentURL)) {
+            // create elements
+            const body = document.body;
+            const bannerWrapper = document.createElement('div');
+            const box = document.createElement('canvas');
+            const boxWrapper = document.createElement('div');
+            const banner = document.createElement('div');
+            const textBox = document.createElement('div');
+            const image = document.createElement('img');
+            const bannerTitle = document.createElement('p');
+            const bannerText = document.createElement('p');
+            const warning = document.createElement('p');
+            const button = document.createElement('button');
 
-    // add background image
-    backgroundImageURL = chrome.runtime.getURL('grid_background.png');
-    banner.style.background = `url(${backgroundImageURL}) repeat`;
+            // link elements to stylesheet
+            bannerWrapper.id = 'warningbanner-wrapper';
+            box.id = 'box';
+            banner.id = 'warningbanner';
+            textBox.id = 'text-box';
+            image.id = 'threatslayer_logo';
+            warning.id = 'warning';
+            bannerTitle.class = 'warningbanner-text';
+            bannerText.class = 'warningbanner-text';
+            button.id = 'dismiss-button';
 
-    // generate logo URL
-    imageURL = chrome.runtime.getURL('threatslayer_logo.png');
-    image.setAttribute('src', imageURL);
+            // add background image
+            backgroundImageURL = chrome.runtime.getURL('grid_background.png');
+            banner.style.background = `url(${backgroundImageURL}) repeat`;
 
-    // "Warning!" / "This website may be malicious." / "Close the tab unless you know this site is safe!"
-    warning.appendChild(document.createTextNode(chrome.i18n.getMessage("warning")));
-    bannerTitle.appendChild(document.createTextNode(chrome.i18n.getMessage("warning_header")));
-    bannerText.appendChild(document.createTextNode(chrome.i18n.getMessage("warning_text")));
+            // generate logo URL
+            imageURL = chrome.runtime.getURL('threatslayer_logo.png');
+            image.setAttribute('src', imageURL);
 
-    // "Trust This Site" button
-    button.innerHTML = '<span id="dismiss-button-text">Trust This Site</span>';
+            // "Warning!" / "This website may be malicious." / "Close the tab unless you know this site is safe!"
+            warning.appendChild(document.createTextNode(chrome.i18n.getMessage("warning")));
+            bannerTitle.appendChild(document.createTextNode(chrome.i18n.getMessage("warning_header")));
+            bannerText.appendChild(document.createTextNode(chrome.i18n.getMessage("warning_text")));
 
-    // build banner and underlying box
-    banner.appendChild(image);
-    banner.append(textBox);
-    textBox.appendChild(warning);
-    textBox.appendChild(bannerTitle);
-    textBox.appendChild(bannerText);
-    textBox.appendChild(button);
-    bannerWrapper.appendChild(banner);
-    boxWrapper.appendChild(box);
+            // "Trust This Site" button
+            button.innerHTML = '<span id="dismiss-button-text">Trust This Site</span>';
 
-    // insert box and banner into page
-    body.insertBefore(bannerWrapper, body.childNodes[0]);
-    body.insertBefore(boxWrapper, body.childNodes[0]);
+            // build banner and underlying box
+            banner.appendChild(image);
+            banner.append(textBox);
+            textBox.appendChild(warning);
+            textBox.appendChild(bannerTitle);
+            textBox.appendChild(bannerText);
+            textBox.appendChild(button);
+            bannerWrapper.appendChild(banner);
+            boxWrapper.appendChild(box);
 
-    // on click to close banner, remove banner and box
-    button.onclick = function () {
-        body.removeChild(bannerWrapper);
-        body.removeChild(boxWrapper);
-    };
+            // insert box and banner into page
+            body.insertBefore(bannerWrapper, body.childNodes[0]);
+            body.insertBefore(boxWrapper, body.childNodes[0]);
+
+            // on click to close banner, remove banner and box
+            button.onclick = function () {
+                chrome.storage.local.get(["allowlist"]).then((result) => {
+                    const allowlist = result.allowlist || [];
+                    const currentURL = document.URL;
+
+                    if (!allowlist.includes(currentURL)) {
+                        allowlist.push(currentURL);
+                        console.log(`Adding ${currentURL} to allowlist`);
+
+                        chrome.storage.local.set({ allowlist }).then(() => {
+                            body.removeChild(bannerWrapper);
+                            body.removeChild(boxWrapper);
+                        })
+                    } else {
+                        body.removeChild(bannerWrapper);
+                        body.removeChild(boxWrapper);
+                    }
+                });
+            };
+        };
+    });
 })();
