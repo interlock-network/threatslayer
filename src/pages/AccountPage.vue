c483804a6781372fe05cb9984bd58762a8c31c29
Dan TS <dan@interlock.network>
2023-07-24 16:31:22 -0700

    AccountPage.vue: adds logic to show and edit allowlist

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,21 +1,32 @@
 <template>
-    <PageBanner msg="">
+    <PageBanner>
         <img class="banner-icon" src="/src/assets/images/account.png">Account info for {{ username }}
     </PageBanner>
     <br />
     <LineOfText msg="Email" bold />
-    <LineOfText msg="">{{ email }}</LineOfText>
+    <LineOfText>{{ email }}</LineOfText>
     <br />
     <br />
     <br />
     <LineOfText msg="Wallet Address" bold />
-    <LineOfText msg="">{{ address }}</LineOfText>
+    <LineOfText mono>{{ address }}</LineOfText>
+    <br />
+    <br />
+    <br />
+    <LineOfText @click="sort" bold>{{ allowlistHeader }}</LineOfText>
+    <div id="url-container">
+        <LineOfText v-for="url in sortedAllowlist" mono>
+            <button @click="clearUrl(url)" class="x-button">X</button>{{ url }}
+        </LineOfText>
+        <LineOfText v-if="!showClearButton" msg="No URLs allowlisted" />
+        <button v-if="showClearButton" @click="clearAllUrls" id="clear-allowlist">Clear all allowlisted URLs</button>
+    </div>
 </template>
 <script>
 import LineOfText from "./components/subcomponents/LineOfText.vue";
 import PageBanner from "./components/subcomponents/PageBanner.vue";
 
-import { getChromeStorage } from '../utilities.js';
+import { getChromeStorage, setChromeStorage } from '../utilities.js';
 
 export default {
     name: 'AccountPage',
@@ -25,15 +36,63 @@ export default {
     },
     data() {
         return {
-            email: '',
-            username: '',
             address: '',
+            allowlist: null,
+            currentSortDir: 'chron', //defaults to chronological / API order
+            email: '',
+            sortedAllowlist: [],
+            username: '',
         };
     },
     mounted() {
         this.getUserInfo();
+        this.getAllowlist();
+    },
+    computed: {
+        allowlistHeader() {
+            const sortInfo = this.currentSortDir === 'asc' ? '▲' :
+                this.currentSortDir === 'desc' ? '▼' : '(click to sort)';
+
+            return `Allowlisted Sites ${sortInfo}`;
+        },
+        // TODO fix this to update correctly
+        showClearButton() {
+            return this.allowlist;
+        },
+        sortedAllowlist() {
+            if (!this.allowlist) return [];
+
+            let result = [];
+
+            if (this.currentSortDir === 'asc') {
+                result = [...this.allowlist].sort((a, b) => a > b ? -1 : 1);
+            } else if (this.currentSortDir === 'desc') {
+                result = [...this.allowlist].sort((a, b) => a > b ? 1 : -1);
+            } else {
+                result = this.allowlist;
+            }
+
+            return result;
+        },
     },
     methods: {
+        async clearAllUrls() {
+            setChromeStorage({ allowlist: null });
+
+            this.allowlist = [];
+        },
+        async clearUrl(urlToClear) {
+            const allowlist = await getChromeStorage('allowlist');
+
+            this.allowlist = allowlist.filter(url => url !== urlToClear);
+
+            setChromeStorage({ allowlist: this.allowlist });
+        },
+        async getAllowlist() {
+            const allowlist = await getChromeStorage('allowlist');
+
+            this.allowlist = allowlist;
+        },
         async getUserInfo() {
             const email = await getChromeStorage('email');
             const username = await getChromeStorage('username');
@@ -42,15 +101,35 @@ export default {
             this.email = email;
             this.username = username;
             this.address = address;
-        }
+        },
+        sort: function () {
+            this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' :
+                this.currentSortDir === 'desc' ? 'chron' : 'asc';
+        },
     }
 }
 </script>
   
 <style>
-.socials-icon {
-    padding-left: 1rem;
-    padding-right: 1rem;
+#clear-allowlist {
+    background-color: #0F0818;
+    border: #BB00FD solid 1px;
+    border-radius: 12px;
+    color: #d0d4d9;
+    font-size: 1.25rem;
+    padding: 0.5rem 0.75rem;
+    width: 400px;
+}
+
+#url-container {
+    height: 25vh;
+    overflow: scroll;
+}
+
+.x-button {
+    background-color: inherit;
+    border: none;
+    color: red;
 }
 </style>
   
