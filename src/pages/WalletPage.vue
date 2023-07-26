cf02af576670556e456352098538aa7af200ed0d
Dan TS <dan@interlock.network>
2023-07-26 16:10:01 -0700

    WalletPage.vue: refactors to use simpler TextComponent

--- src/pages/WalletPage.vue
+++ src/pages/WalletPage.vue
@@ -2,28 +2,29 @@
     <PageBanner msg="Create Wallet">
         <img class="banner-icon" src="/src/assets/images/wallet.png">
     </PageBanner>
-    <LineOfText instruction>
-        <span>Already have a wallet?<button class="login-button" @click="selectPage('earn')">Register</button></span>
-    </LineOfText>
+    <TextComponent msg="Already have a wallet?" instruction /><button class="login-button"
+        @click="selectPage('earn')">Register</button>
     <br />
     <br />
-    <LineOfText msg="In order to use ThreatSlayer, you will need to create an Aleph Zero-compatible wallet." />
+    <TextComponent msg="In order to use ThreatSlayer, you will need to create an Aleph Zero-compatible wallet." />
     <br />
-    <LineOfText msg="Please click 'Create Wallet' to be redirected to a Chrome browser extension to create a SubWallet." />
+    <br />
+    <TextComponent
+        msg="Please click 'Create Wallet' to be redirected to a Chrome browser extension to create a SubWallet." />
     <br />
     <br />
     <WalletButton tabindex="2" />
 </template>
 <script>
-import LineOfText from "./components/LineOfText.vue";
 import PageBanner from "./components/PageBanner.vue";
+import TextComponent from "./components/TextComponent.vue";
 import WalletButton from "./components/WalletButton.vue";
 
 export default {
     name: 'AboutPage',
     components: {
-        LineOfText,
         PageBanner,
+        TextComponent,
         WalletButton
     },
     props: {
