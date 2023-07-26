e82e108be57818c44abe657062712df4f29529c7
Dan TS <dan@interlock.network>
2023-07-26 15:20:53 -0700

    FAQPage.vue: updates questions, removes cruft

--- src/pages/FAQPage.vue
+++ src/pages/FAQPage.vue
@@ -5,24 +5,16 @@
     <br />
     <LineOfText msg="What data does ThreatSlayer Collect?" bold />
     <LineOfText
-        msg="ThreatSlayer collects no personal information. The only thing sent to our servers is a URL, so we can scan it with our threat-detecting AI. These URLs are anonymized and there is no way to connect any user with any URL." />
-    <br />
-    <br />
-    <LineOfText msg="Does it work on mobile?" bold />
-    <LineOfText
-        msg="Not yet! Mobile browsers are designed to make it difficult for independent companies to add extensions to them." />
-    <br />
-    <br />
-    <LineOfText msg="Which browsers does ThreatSlayer work on?" bold />
-    <LineOfText msg="Chrome, Brave, Edge, and other Chrome-compatible browsers. FireFox coming soon!" />
-    <br />
+        msg="URLs are sent to our classifier so we can scan them with our threat-detecting AI. If you are registered, we associate URLs to your account to reward you for the URLs you get scanned." />
     <br />
     <LineOfText msg="Is ThreatSlayer open source?" bold />
-    <LineOfText>
-        <span>Absolutely. You can look at our <a href="https://github.com/interlock-network/threatslayer"
-                target="_blank">source
-                code on GitbHub</a> and see exactly what it does.</span>
-    </LineOfText>
+    <div>
+        <LineOfText>
+            <span>Absolutely. You can look at our <a href="https://github.com/interlock-network/threatslayer"
+                    target="_blank">source
+                    code on GitbHub</a> and see exactly what it does.</span>
+        </LineOfText>
+    </div>
 </template>
 <script>
 import LineOfText from "./components/subcomponents/LineOfText.vue";
@@ -33,26 +25,13 @@ export default {
     components: {
         LineOfText,
         PageBanner
-    },
-    data() {
-        return {
-
-        };
-    },
-    mounted() {
-
-    },
-    computed: {
-
-    },
-    methods: {}
+    }
 }
 </script>
-  
+
 <style>
 .socials-icon {
     padding-left: 1rem;
     padding-right: 1rem;
 }
 </style>
-  
