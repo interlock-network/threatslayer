cbf0f6b14701aaaeab1e2763cba76ab005dcfc3d
Dan TS <dan@interlock.network>
2023-07-27 16:27:47 -0700

    FAQPage.vue: completes draft FAQ content

--- src/pages/FAQPage.vue
+++ src/pages/FAQPage.vue
@@ -3,19 +3,36 @@
         <img class="banner-icon" src="/src/assets/images/faq.png">
     </PageBanner>
     <br />
-    <TextComponent msg="What data does ThreatSlayer Collect?" bold /><br />
     <br />
-    <TextComponent
-        msg="URLs are sent to our classifier so we can scan them with our threat-detecting AI. If you are registered, we associate URLs to your account to reward you for the URLs you get scanned." />
-    <br />
-    <br />
-    <br />
-    <TextComponent msg="Is ThreatSlayer open source?" bold /><br />
-    <div>
-        <LineOfText>
-            <span>Absolutely. You can look at our <a href="https://github.com/interlock-network/threatslayer"
-                    target="_blank">source code on GitbHub</a> and see exactly what it does.</span>
-        </LineOfText>
+    <div style="padding-right: 2rem;">
+        <TextComponent msg="How does ThreatSlayer protect me?" bold /><br />
+        <br />
+        <TextComponent
+            msg="ThreatSlayer uses threat-detecting AI to instantly block malicious sites and prevent them from stealing your data." />
+        <br />
+        <br />
+        <br />
+        <TextComponent msg="How does ThreatSlayer reward me with $ILOCK?" bold /><br />
+        <br />
+        <TextComponent
+            msg="ThreatSlayer users have a chance to get rewarded for every URL they visit. The more sites you browse, the more chances you have to get $ILOCK." />
+        <br />
+        <br />
+        <br />
+        <TextComponent msg="What data does ThreatSlayer Collect?" bold /><br />
+        <br />
+        <TextComponent
+            msg="We scan your browser's URLs with our threat-detecting AI. We associate URLs with your account so we can reward you with $ILOCK. That's it!" />
+        <br />
+        <br />
+        <br />
+        <TextComponent msg="Is ThreatSlayer open source?" bold /><br />
+        <div>
+            <LineOfText>
+                <span>Absolutely. You can look at our <a href="https://github.com/interlock-network/threatslayer"
+                        target="_blank">source code on GitbHub</a> and see exactly what it does.</span>
+            </LineOfText>
+        </div>
     </div>
     <br />
     <br />
