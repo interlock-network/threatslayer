057be37f99d81ccd9b0fa7f8a0455d37fd0043e3
Dan TS <dan@interlock.network>
2023-08-02 15:37:59 -0700

    CreateUserButton.vue: centralizes button text generation, fixes disabling button

--- src/pages/components/buttons/CreateUserButton.vue
+++ src/pages/components/buttons/CreateUserButton.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <button @click="submitForm()" class="submit-button" :class="computedClass" :disabled="submitting || submitted">
+        <button @click="submitForm()" class="submit-button" :class="computedClass" :disabled="disabled">
             {{ submitButtonText }}
         </button>
-        <!-- TODO add error message here -->
+        <TextComponent v-for="errorMessage in errorArr" :msg="errorMessage" error />
     </div>
 </template>
 
 <script>
+import TextComponent from "../TextComponent.vue";
+
 import axios from "axios";
 import { baseUrl, setChromeStorage } from '../../../utilities.js';
 
@@ -23,20 +25,21 @@ export default {
         unitedStates: Boolean,
         username: String,
     },
+    components: {
+        TextComponent
+    },
     data() {
         return {
-            error: false,
             errorArr: [],
-            submitButtonText: 'Submit',
-            submitting: false,
-            submitted: false
+            submitted: false,
+            submitting: false
         }
     },
     computed: {
         computedClass() {
             let className = '';
 
-            if (this.error) {
+            if (this.errorArr.length) {
                 className = 'submit-button-error';
             } else {
                 className = 'submit-button-active';
@@ -44,39 +47,50 @@ export default {
 
             return className;
         },
+        submitButtonText() {
+            let result = '';
+
+            if (this.loggedIn) {
+                result = 'Success!';
+            } else if (this.loggingIn) {
+                result = 'Waiting...';
+            } else if (this.errorArr.length) {
+                result = "Try again later";
+            } else {
+                result = 'Submit';
+            }
+
+            return result;
+        },
+        disabled() {
+            const { address, email, password, submitting, submitted, termsOfService: terms_of_service, unitedStates: united_states, username } = this;
+
+            return (!address?.length || !email?.length || !password?.length || submitting || submitted || !terms_of_service || !united_states || !username?.length)
+        }
     },
     methods: {
         async submitForm() {
             const { address, email, password, referrer, termsOfService: terms_of_service, unitedStates: united_states, username } = this;
             const key = 'threatslayer-api-key';
             this.submitting = true;
-            this.submitButtonText = 'Waiting...';
-
-            if (!address.length || !email.length || !password.length || !terms_of_service || !united_states || !username.length) {
-                this.submitButtonText = 'Incomplete';
-                this.error = true;
-
-                return;
-            }
 
             // TODO update with endpoint URL
             const response = await axios.post(`${baseUrl}/user-create`, { address, email, key, password, referrer, terms_of_service, united_states, username })
-                .then(res => {
-                    console.log('res', res);
-                    return res;
-                }).catch(err => {
-                    console.log('Error registering:', err);
-                    // TODO update error object
-                    return { errors: ['Error submitting to API.'] }
-                });
+                .then(res => res)
+                .catch(err => err);
 
-            const errors = response.errors;
+            const { errors = [], response: { status = 200, statusText = '' } = {} } = response;
 
-            // TODO test this
-            if (!errors?.length) {
+            if (status >= 400) {
+                console.log(`Create user error. Code: ${status}, status text: ${statusText}`);
+
+                this.submitted = false;
+                this.submitting = false;
+                this.errorArr.push(`Error: ${statusText}`);
+            } else if (!errors?.length) {
+                // TODO test this
                 this.submitted = true;
                 this.submitting = false;
-                this.submitButtonText = 'Success';
 
                 // set API key with user's unique key
                 setChromeStorage({ address });
@@ -99,9 +113,7 @@ export default {
             } else {
                 console.log('Error submitting registration:', errors);
 
-                this.error = true;
                 this.errorArr = [errors, ...this.errorArr];
-                this.submitButtonText = 'Try again later';
             }
         }
     }
