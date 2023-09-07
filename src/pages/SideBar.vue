b8349179436bf25ac11c9769c1b0817e6d13ea27
Dan TS <dan@interlock.network>
2023-09-07 15:24:22 -0700

    SideBar.vue: adds i18n, fixes bug in toggle login

--- src/pages/SideBar.vue
+++ src/pages/SideBar.vue
@@ -3,43 +3,45 @@
         <img id="threatslayer-logo" src="/src/assets/images/threatslayer_logo.png">
         <div id="sidebar-earn" v-if="showRegisterPage" class="sidebar-item"
             :class="currentPage === 'earn' ? 'selected-sidebar-item' : ''" @click="selectPage('earn')">
-            <img class="sidebar-icon" src="/src/assets/images/start_earning.png"><span class=sidebar-text>Start
-                Earning</span>
+            <img class="sidebar-icon" src="/src/assets/images/start_earning.png"><span class=sidebar-text>{{
+                $i18n('start_earning') }}</span>
         </div>
         <div id="sidebar-wallet" v-if="showRegisterPage" class="sidebar-item"
             :class="currentPage === 'wallet' ? 'selected-sidebar-item' : ''" @click="selectPage('wallet')">
-            <img class="sidebar-icon" src="/src/assets/images/wallet.png"><span class=sidebar-text>Create Wallet</span>
+            <img class="sidebar-icon" src="/src/assets/images/wallet.png"><span class=sidebar-text>{{ $i18n('create_wallet')
+            }}</span>
         </div>
         <div id="sidebar-login" v-if="showLoginPage" class="sidebar-item"
             :class="currentPage === 'login' ? 'selected-sidebar-item' : ''" @click="selectPage('login')">
             <div style="position: relative">
-                <img class="sidebar-icon" src="/src/assets/images/login.png"><span class="sidebar-text">Login</span>
+                <img class="sidebar-icon" src="/src/assets/images/login.png"><span class="sidebar-text">{{ $i18n('login')
+                }}</span>
             </div>
         </div>
         <div v-if="urlToStake" id="sidebar-staking" class="sidebar-item"
             :class="currentPage === 'staking' ? 'selected-sidebar-item' : ''" @click="selectPage('staking')">
-            <img class="sidebar-icon" src="/src/assets/images/slay_count.png">Security Staking
+            <img class="sidebar-icon" src="/src/assets/images/slay_count.png">{{ $i18n('security_staking') }}
         </div>
         <div id="sidebar-slayCount" class="sidebar-item" :class="currentPage === 'slayCount' ? 'selected-sidebar-item' : ''"
             @click="selectPage('slayCount')">
-            <img class="sidebar-icon" src="/src/assets/images/slay_count.png">Slay Count
+            <img class="sidebar-icon" src="/src/assets/images/slay_count.png">{{ $i18n('slay_count') }}
         </div>
         <div id="sidebar-about" class="sidebar-item" :class="currentPage === 'about' ? 'selected-sidebar-item' : ''"
             @click="selectPage('about')">
-            <img class="sidebar-icon" src="/src/assets/images/about.png">About Us
+            <img class="sidebar-icon" src="/src/assets/images/about.png">{{ $i18n('about_us') }}
         </div>
         <div id="sidebar-faq" class="sidebar-item" :class="currentPage === 'faq' ? 'selected-sidebar-item' : ''"
             @click="selectPage('faq')">
-            <img class="sidebar-icon" src="/src/assets/images/faq.png">FAQ
+            <img class="sidebar-icon" src="/src/assets/images/faq.png">{{ $i18n('faq') }}
         </div>
         <div id="sidebar-privacy" class="sidebar-item" :class="currentPage === 'privacy' ? 'selected-sidebar-item' : ''"
             @click="selectPage('privacy')">
-            <img class="sidebar-icon" src="/src/assets/images/privacy.png">Privacy <img class="link-button-icon"
-                src="/src/assets/images/external_link.png">
+            <img class="sidebar-icon" src="/src/assets/images/privacy.png">{{ $i18n('privacy') }}<img
+                class="link-button-icon" src="/src/assets/images/external_link.png">
         </div>
         <div id="sidebar-options" class="sidebar-item" :class="currentPage === 'options' ? 'selected-sidebar-item' : ''"
             @click="selectPage('options');">
-            <img class="sidebar-icon" src="/src/assets/images/settings.png">Options
+            <img class="sidebar-icon" src="/src/assets/images/settings.png">{{ $i18n('options') }}
         </div>
         <div v-if="showLogoutButton" id="bottom-sidebar-items">
             <div id="sidebar-account" class="sidebar-item" :class="currentPage === 'account' ? 'selected-sidebar-item' : ''"
@@ -104,17 +106,17 @@ export default {
         },
         // TODO delete this
         async _toggleLoggedIn() {
+            const loggedIn = !this.loggedIn;
             const address = this.loggedIn ? null : '5GrpknVvGGrGH3EFuURXeMrWHvbpj3VfER1oX5jFtuGbfzCE';
             const email = this.loggedIn ? null : 'alice@example.com';
-            const key = this.loggedIn ? null : 'threatslayer-api-key';
-            const loggedIn = !this.loggedIn;
+            const apiKey = this.loggedIn ? null : 'threatslayer-api-key';
             const username = this.loggedIn ? null : 'alice';
 
-            const addressSet = await setChromeStorage({ address }, `Set address as ${address}`, `Error setting key as ${address}`);
-            const emailSet = await setChromeStorage({ email }, `Set email as ${email}`, `Error setting email as ${email}`);
-            const keySet = await setChromeStorage({ apiKey: key }, `Set key as ${key}`, `Error setting key as ${key}`);
-            const loggedInSet = await setChromeStorage({ loggedIn }, `Set loggedIn as ${loggedIn}`, `Error setting loggedIn as ${loggedIn}`);
-            const usernameSet = await setChromeStorage({ username }, `Set username as ${username}`, `Error setting key as ${username}`);
+            const addressSet = await setChromeStorage({ address });
+            const emailSet = await setChromeStorage({ email });
+            const keySet = await setChromeStorage({ apiKey });
+            const loggedInSet = await setChromeStorage({ loggedIn });
+            const usernameSet = await setChromeStorage({ username });
 
             if (emailSet && keySet && loggedInSet && usernameSet && addressSet) this.checkState();
         },
