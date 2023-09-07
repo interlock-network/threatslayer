4aa52236eda3ae1e7959b0f20e0ea5dfcac96ebd
Dan TS <dan@interlock.network>
2023-09-07 15:23:42 -0700

    main.js: injects internationalization / i18n to Vue.js files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import i18n from 'vue-plugin-webextension-i18n';
 
 if (process.env.NODE_ENV === 'development') {
-    const { worker } = require('./mocks/browser')
-    worker.start()
+    const { worker } = require('./mocks/browser');
+    worker.start();
 }
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app');
