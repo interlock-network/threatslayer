94bfcb634f79611579d887dda277fdaa43317175
Dan TS <dan@interlock.network>
2023-09-08 15:16:27 -0700

    WarningTextBox.vue: adds a text box for various warnings

---
+++ src/pages/components/WarningTextBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="warning-container">
+        <TextComponent :msg="msg" />
+        <slot />
+    </div>
+</template>
+
+<script>
+import TextComponent from "./TextComponent.vue";
+
+export default {
+    name: "WarningTextBox",
+    props: {
+        msg: String
+    },
+    components: {
+        TextComponent
+    },
+};
+</script>
+
+<style></style>
