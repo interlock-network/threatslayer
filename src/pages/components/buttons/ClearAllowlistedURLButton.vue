f77cd6e9eaa2f41606baa7ed8440e80d395602e1
Dan TS <dan@interlock.network>
2023-09-08 15:19:06 -0700

    ClearAllowlistedURLButton.vue: accounts for if user is not logged in

--- src/pages/components/buttons/ClearAllowlistedURLButton.vue
+++ src/pages/components/buttons/ClearAllowlistedURLButton.vue
@@ -16,14 +16,13 @@ export default {
     methods: {
         async clearUrl() {
             const { apiKey, url } = this;
-            console.log('apiKey', apiKey);
-            console.log('url', url);
-            // TODO add allowlist code here
-            // TODO update URL
-            const response = await axios.post(`${baseUrl}/site-forget`, { key: apiKey, url })
-                .then(res => res)
-                .catch(err => err);
 
+            // users not logged in will not have an apiKey here
+            if (apiKey?.length) {
+                const response = await axios.post(`${baseUrl}/site-forget`, { key: apiKey, url })
+                    .then(res => res)
+                    .catch(err => err);
+            }
             console.log('response', response);
 
             // if cleared URL is the same url as the one to stake, clear it
