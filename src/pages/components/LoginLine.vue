559a6194a5486477f077434691b837651d7c5b1c
Dan TS <dan@interlock.network>
2023-09-08 14:07:03 -0700

    LoginLine.vue: Extracts the prompt to login with link to page

---
+++ src/pages/components/LoginLine.vue
@@ -0,0 +1,20 @@
+<template>
+    <TextComponent :msg="$i18n('already_have_one')" subinstruction /><button class="login-button"
+        @click="selectPage('login')">{{ $i18n('login') }}</button><br />
+</template>
+
+<script>
+import TextComponent from "./TextComponent.vue";
+
+export default {
+    name: "LoginLine",
+    props: {
+        selectPage: Function,
+    },
+    components: {
+        TextComponent
+    }
+};
+</script>
+
+<style></style>
