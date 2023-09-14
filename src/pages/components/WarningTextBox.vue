6617ef676701382417187b0d2407db45d86081f1
Dan TS <dan@interlock.network>
2023-09-14 16:24:53 -0700

    WarningTextBox.vue: moves class for this vue file here

--- src/pages/components/WarningTextBox.vue
+++ src/pages/components/WarningTextBox.vue
@@ -19,4 +19,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.warning-container {
+    border: yellow solid 1px;
+    border-radius: 0.5rem;
+    font-size: 1rem;
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+    width: 380px;
+}
+</style>
