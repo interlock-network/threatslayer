a80d675d286da57a6b8ba9433dc7f3c0f90ccb3b
Dan TS <dan@interlock.network>
2023-09-16 14:50:13 -0700

    WalletList.vue: extracts list of wallet addresses to show on WalletInfoModal.vue

---
+++ src/pages/components/WalletList.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- TODO update translation here -->
+    <div v-if="azeroAddress?.length && changeAddressSelected">
+        <TextComponent msg="Aleph Zero-compatible Wallet Address" bold /> <br />
+        <TextComponent :msg="azeroAddress" mono /><br />
+    </div>
+    <br />
+    <!-- TODO update translation here -->
+    <div v-if="pdotAddress?.length && changeAddressSelected">
+        <TextComponent msg="Moonbeam-compatible Wallet Address" bold /> <br />
+        <TextComponent v-if="pdotAddress?.length && changeAddressSelected" :msg="pdotAddress" mono /><br />
+    </div>
+</template>
+
+<script>
+import TextComponent from "./TextComponent.vue";
+
+export default {
+    name: "WalletList",
+    props: {
+        azeroAddress: String,
+        changeAddressSelected: Boolean,
+        pdotAddress: String,
+    },
+    components: {
+        TextComponent,
+    }
+};
+</script>
+
+<style></style>
