a1f4a4e1b2b738fcd1835cb55a7655ca7e91439d
Dan TS <dan@interlock.network>
2023-09-26 15:20:57 -0700

    CreateWalletLine.vue: extracts instruction and link to create new wallet

---
+++ src/pages/components/CreateWalletLine.vue
@@ -0,0 +1,20 @@
+<template>
+    <TextComponent :msg="$i18n('dont_have_one')" subinstruction /><button class="login-button"
+        @click="selectPage('wallet')">{{ $i18n('create_one') }}</button>
+</template>
+
+<script>
+import TextComponent from "./TextComponent.vue";
+
+export default {
+    name: "CreateWalletLine",
+    props: {
+        selectPage: { type: Function, required: true },
+    },
+    components: {
+        TextComponent
+    }
+};
+</script>
+
+<style></style>
