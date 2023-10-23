b7dceac78b42f917f4bf81268e32525120b55f7f
Dan TS <dan@interlock.network>
2023-10-23 16:10:59 -0700

    LogoutButton.vue: extracted API call * Logging out now happens entirely locally

--- src/pages/components/buttons/LogoutButton.vue
+++ src/pages/components/buttons/LogoutButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sidebar-item" :disabled="disabled" @click="submitLogout">
+    <div class="sidebar-item" :disabled="disabled" @click="logout">
         <img class="sidebar-icon" src="/src/assets/images/logout.png">
         <span>{{ $i18n(logoutButtonText) }}
         </span><br />
@@ -11,23 +11,19 @@
     </div>
 </template>
 <script>
-import axios from "axios";
-import { baseUrl, clearChromeStorage, extractFromError, submitButtonLabels, setChromeStorage } from '../../../utilities.js';
+import { clearChromeStorage, setChromeStorage } from '../../../utilities.js';
 
 export default {
     name: "LogoutButton",
     props: {
         checkState: { type: Function, required: true },
-        apiKey: { type: String, default: '' },
-        selectPage: { type: Function, required: true },
-        username: { type: String, default: '' }
+        selectPage: { type: Function, required: true }
     },
     data() {
         return {
             clickedOnce: false,
             errorArr: [],
             loggedOut: false,
-            loggingOut: false,
             status: 200
         }
     },
@@ -36,78 +32,53 @@ export default {
             return !this.clickedOnce ? 'hidden-sidebar-cancel' : 'visible-sidebar-cancel';
         },
         disabled() {
-            const { loggedOut, loggingOut } = this;
-
-            return loggedOut || loggingOut;
+            return this.loggedOut;
         },
         logoutButtonText() {
-            const { clickedOnce, errorArr, loggedOut: submitted, loggingOut: submitting, status } = this;
+            const { clickedOnce, loggedOut } = this;
 
-            if (clickedOnce && !(submitted || submitting)) {
-                return 'confirm_are_you_sure';
+            let result = '';
+
+            if (clickedOnce && !(loggedOut)) {
+                result = 'confirm_are_you_sure';
+            } else if (loggedOut) {
+                result = 'logged_out';
             } else {
-                const initialMsg = 'logout';
-                const submittedMsg = 'logged_out';
-                const submittingMsg = 'logging_out';
-
-                return submitButtonLabels(
-                    { errorArr, initialMsg, submitting, submittingMsg, submitted, submittedMsg, submitting, status }
-                );
+                result = 'logout';
             }
+
+            return result;
         }
     },
     methods: {
         cancelLogout() {
             this.clickedOnce = false;
         },
-        submitLogout() {
+        async logout() {
             // user must confirm before they are logged out
             if (!this.clickedOnce) {
                 this.clickedOnce = true;
             } else {
-                const { apiKey, username } = this;
+                this.loggedOut = true;
 
-                this.errorArr = [];
-                this.loggingOut = true;
+                const azeroAddressClearedFromState = await clearChromeStorage('azeroAddress');
+                const emailClearedFromState = await clearChromeStorage('email');
+                const keyClearedFromState = await clearChromeStorage('apiKey');
+                const pdotAddressClearedFromState = await clearChromeStorage('pdotAddress');
+                const usernameClearedFromState = await clearChromeStorage('username');
 
-                axios.post(`${baseUrl}/user-logout`, { key: apiKey, username })
-                    .then(response => {
-                        const { status } = response.data;
+                // this happens last
+                const loggedOut = await setChromeStorage({ loggedIn: false });
 
-                        this.status = status;
-                    })
-                    .catch(error => {
-                        const { errors, status } = extractFromError(error);
+                const loggedOutSynched = azeroAddressClearedFromState && emailClearedFromState && keyClearedFromState
+                    && loggedOut && pdotAddressClearedFromState && usernameClearedFromState;
 
-                        this.status = status;
-
-                        console.log(`Logout error. Status: ${status}. Error: ${errors}`);
-                    })
-                    .finally(async () => {
-                        // regardless of whether there are errors, users should be able to logout of the extension
-                        // this prevents users from being unable to logout, e.g. because they logged in from a different
-                        // installation on another browser and their API key changed
-                        this.loggingOut = false;
-                        this.loggedOut = true;
-
-                        const azeroAddressClearedFromState = await clearChromeStorage('azeroAddress');
-                        const emailClearedFromState = await clearChromeStorage('email');
-                        const keyClearedFromState = await clearChromeStorage('apiKey');
-                        const pdotAddressClearedFromState = await clearChromeStorage('pdotAddress');
-                        const usernameClearedFromState = await clearChromeStorage('username');
-                        // this happens last
-                        const loggedOut = await setChromeStorage({ loggedIn: false });
-
-                        const loggedOutSynched = azeroAddressClearedFromState && emailClearedFromState && keyClearedFromState
-                            && loggedOut && pdotAddressClearedFromState && usernameClearedFromState;
-
-                        if (!loggedOutSynched) {
-                            this.errorArr.push('Error logging in. Please try again later.')
-                        }
-                        // navigate to user page after logging out, successfully or not
-                        this.selectPage('login');
-                        this.checkState();
-                    });
+                if (!loggedOutSynched) {
+                    this.errorArr.push('Error logging in. Please try again later.')
+                }
+                // navigate to user page after logging out, successfully or not
+                this.selectPage('login');
+                this.checkState();
             }
         }
     }
