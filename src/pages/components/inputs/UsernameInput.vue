90d1907ff011bdfb676549e43780a02bad4d2113
Dan TS <dan@interlock.network>
2023-10-23 16:03:51 -0700

    UsernameInput.vue: extracts helper functions to here from utilities.js

--- src/pages/components/inputs/UsernameInput.vue
+++ src/pages/components/inputs/UsernameInput.vue
@@ -6,7 +6,6 @@
 
 <script>
 import { debounce } from 'debounce';
-import { findNonAlphanumericChars, validateUsername } from "../../../utilities";
 
 import ErrorMessage from "../ErrorMessage.vue";
 
@@ -37,17 +36,58 @@ export default {
         }
     },
     methods: {
+        findNonAlphanumericChars(str) {
+            let result = [];
+
+            if (str?.length) {
+                const regex = /[^A-Za-z0-9]/g;
+                const matchesArr = str.match(regex);
+
+                if (matchesArr?.length) {
+                    const illegalChars = matchesArr.map(char =>
+                        char === ' ' ? 'space' :
+                            char === ',' ? 'comma' : char
+                    );
+
+                    // to remove duplicate chars and stringify
+                    result = [...new Set(illegalChars)].join(', ');
+                }
+            }
+
+            return result;
+        },
+        findUsernameErrors(username = '') {
+            let result = '';
+
+            // number of characters
+            const maxLength = 16;
+            const minLength = 5;
+
+            const allowedCharsRegex = /^[a-zA-Z0-9_]+$/;
+            const containsIllegalChars = !allowedCharsRegex.test(username);
+
+            if (!username.length) {
+                result = ''; // no change, empty string === valid
+            } else if (username.length < minLength) {
+                result = 'warning_username_too_short';
+            } else if (username.length > maxLength) {
+                result = 'warning_username_too_long';
+            } else if (containsIllegalChars) {
+                result = 'warning_username_contains_illegal_characters';
+            }
+
+            return result;
+        },
         validateUsername: debounce(function () {
             const username = this.username;
-            const errorMessage = validateUsername(username);
-            const illegalChars = findNonAlphanumericChars(username);
+            const errorMessage = this.findUsernameErrors(username);
+            const illegalChars = this.findNonAlphanumericChars(username);
             const hasError = !!errorMessage.length;
 
             this.errorMessage = errorMessage;
             this.illegalChars = illegalChars;
             this.$emit('currentUsername', username);
             this.$emit('usernameHasError', hasError);
-
         }, 250)
     }
 }
