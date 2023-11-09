f5472da43f5044573fd7e26a821726c8110add6b
Dan TS <dan@interlock.network>
2023-11-09 10:19:40 -0800

    WalletList.vue: adds logic to delete a wallet address

--- src/pages/components/WalletList.vue
+++ src/pages/components/WalletList.vue
@@ -1,8 +1,9 @@
 <template>
     <div v-if="azeroAddress?.length && !changeAddressSelected">
         <TextComponent :msg="$i18n('wallet_address')" bold /> <br />
-        <TextComponent :msg="azeroAddress" wallet /><br />
-        <br />
+        <button id="clear-wallet-button"><img @click="deleteWalletSelected(true)" class="sidebar-icon"
+                src="/src/assets/images/x-icon.png"></button>
+        <TextComponent :msg="azeroAddress" wallet /><br /><br />
     </div>
 </template>
 
@@ -11,14 +12,38 @@ import TextComponent from "./TextComponent.vue";
 
 export default {
     name: "WalletList",
-    props: {
-        azeroAddress: { type: String, default: '' },
-        changeAddressSelected: { type: Boolean, required: true }
-    },
     components: {
         TextComponent
+    },
+    props: {
+        azeroAddress: { type: String, default: '' },
+        changeAddressSelected: { type: Boolean, required: true },
+        deleteWalletSelected: { type: Function, required: true }
     }
 };
 </script>
 
-<style></style>
+<style>
+#clear-wallet-button {
+    background: inherit;
+    border: none;
+    padding-bottom: 3px;
+    padding-left: 5px;
+}
+
+#delete-address-button {
+    background-color: #0F0818;
+    border: none;
+    color: #9000CB;
+    font-size: 1rem;
+    height: 2rem;
+    margin-bottom: 2rem;
+    padding: 0.5rem 0.75rem;
+    pointer-events: initial;
+    width: 400px;
+}
+
+#delete-address-button:hover {
+    background-color: #4A0064;
+}
+</style>
