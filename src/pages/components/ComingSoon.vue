b09e5f3570b084a4fc631073aef6175c2f223eeb
Dan TS <dan@interlock.network>
2023-12-15 10:11:05 -0800

    ComingSoon.vue: creates new "Coming Soon" vue component

---
+++ src/pages/components/ComingSoon.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <span id="coming-soon">{{ $i18n('coming_soon') }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ComingSoon"
+};
+</script>
+
+<style>
+#coming-soon {
+    color: #FEAB2A;
+    display: inline-block;
+    font-size: 2.25rem;
+    font-weight: bold;
+    margin-bottom: 1.75rem;
+    margin-top: 1.25rem;
+}
+</style>
